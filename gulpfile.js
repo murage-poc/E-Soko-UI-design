20c8e00032ed6d781f6f37470b65b9d1ec7c1ddf
mimidots <murage@mimidots.com>
2020-03-12 23:26:36 +0300

    added icon fonts and included way to copy builds on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,44 @@
-let gulp=require('gulp');
-let sass=require('gulp-sass');
-let uglify=require('gulp-uglify');
-let autoprefixer=require('gulp-autoprefixer');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let uglify = require('gulp-uglify');
+let autoprefixer = require('gulp-autoprefixer');
 
-let tsify=require('tsify');
-let source=require('vinyl-source-stream');
-let browserify=require('browserify');
-let vinylBuffer=require('vinyl-buffer');
+let tsify = require('tsify');
+let source = require('vinyl-source-stream');
+let browserify = require('browserify');
+let vinylBuffer = require('vinyl-buffer');
 
 
-let paths={
-    ts:{
-        scr:'./assets/ts/*.ts',
-        dest:'./dist/assets/js/',
-        entries:['./assets/ts/index.ts']
+let paths = {
+    ts: {
+        scr: './assets/ts/*.ts',
+        dest: './dist/assets/js/',
+        entries: ['./assets/ts/index.ts']
     },
-    sass:{
-        scr:'./assets/sass/*.sass',
-        dest:'./dist/assets/css/'
+    sass: {
+        scr: './assets/sass/*.sass',
+        dest: './dist/assets/css/'
     },
-    pages:{
-        scr:'./*.html',
-        dest:'./dist/'
+    pages: {
+        scr: './*.html',
+        dest: './dist/'
     },
-    images:{
-        scr:'./assets/images/*.*',
-        dest:'./dist/assets/images/'
+    font: {
+        scr: './assets/font/*',
+        dest: './dist/assets/font'
+    },
+    images: {
+        scr: './assets/images/*.*',
+        dest: './dist/assets/images/'
     }
 };
 
-let tsOptions={
+let tsOptions = {
     "target": "es3",
     "sourceMap": false,
     "alwaysStrict": true,
     "moduleResolution": "node",
-    "noImplictyAny":true
+    "noImplictyAny": true
 };
 
 function copyPages() {
@@ -42,30 +46,35 @@ function copyPages() {
         .pipe(gulp.dest(paths.pages.dest))
 }
 
+function copyFonts() {
+    return gulp.src(paths.font.scr)
+        .pipe(gulp.dest(paths.font.dest))
+}
+
 function copyImages() {
     return gulp.src(paths.images.scr)
         .pipe(gulp.dest(paths.images.dest))
 }
 
-function sassTask(){
+function sassTask() {
     return gulp.src(paths.sass.scr)
-        .pipe(sass({includePaths:['node_modules']}))
-        .pipe(autoprefixer({cascade:true}))
+        .pipe(sass({includePaths: ['node_modules']}))
+        .pipe(autoprefixer({cascade: true}))
         .pipe(gulp.dest(paths.sass.dest))
 }
 
 function jsBundler() {
     return browserify({
-        entries:paths.ts.entries,
-        basedir:'.',
-        cache:{},
-        packageCache:{}
+        entries: paths.ts.entries,
+        basedir: '.',
+        cache: {},
+        packageCache: {}
     })
 }
 
 function tsTask() {
     return jsBundler()
-        .plugin(tsify,tsOptions)
+        .plugin(tsify, tsOptions)
         .bundle()
         .pipe(source('app-bundle.js'))
         .pipe(vinylBuffer())
@@ -73,14 +82,15 @@ function tsTask() {
         .pipe(gulp.dest(paths.ts.dest))
 }
 
-function watch(){
+function watch() {
     //watch changes
-    gulp.watch([paths.sass.scr],sassTask);
-    gulp.watch([paths.ts.scr],tsTask);
-    gulp.watch([paths.images.scr],copyImages);
-    gulp.watch([paths.pages.scr],copyPages);
+    gulp.watch([paths.sass.scr], sassTask);
+    gulp.watch([paths.ts.scr], tsTask);
+    gulp.watch([paths.images.scr], copyImages);
+    gulp.watch([paths.pages.scr], copyPages);
+    gulp.watch([paths.font.scr], copyFonts);
 }
 
 
-exports.default=gulp.parallel(watch,sassTask,tsTask,copyPages,copyImages);
+exports.default = gulp.parallel(watch, sassTask, tsTask, copyPages, copyImages, copyFonts);
 
